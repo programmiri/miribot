f446bad372e012f7c25e7b46232cc33045a82f9e
programmiri <programmiri@gmail.com>
2016-05-29 16:00:32 +0200

    adding parking.js 🚗

---
+++ scripts/parking.js
@@ -0,0 +1,21 @@
+// Description:
+//   Checks if a parking slot (google cal) is available.
+//
+// Commands:
+//   miribot Parkplatz frei
+
+var parkingcheck = require('../lib/ical-parking-check');
+var calLinkParking1 = process.env.HUBOT_PARKING_ICAL_LINK1;
+var calLinkParking2 = process.env.HUBOT_PARKING_ICAL_LINK2;
+
+module.exports = function(robot) {
+  robot.respond(/parkplatz frei/i, function(res) {
+    parkingcheck(calLinkParking1, function(result) {
+      res.send('Parkplatz 1 ' + result);
+    });
+
+    parkingcheck(calLinkParking2, function(result) {
+      res.send('Parkplatz 2 ' + result);
+    });
+  });
+};
