0b23dede2c307b811acdd4283667cd61a99db9b2
programmiri <programmiri@gmail.com>
2016-09-22 17:33:16 +0200

    typo

---
+++ scripts/insider-one.js
@@ -0,0 +1,12 @@
+// Description:
+//   A special little thing about blanks
+//
+// Commands:
+//   @benda
+
+module.exports = function(robot) {
+  robot.hear(/benda/i, function(res) {
+    res.send('Du meinst doch BRENDA?!');
+  });
+};
+
