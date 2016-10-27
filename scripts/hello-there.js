20c9bf4785061d2b3b687d2c456836a53b2831e6
programmiri <programmiri@gmail.com>
2016-10-27 13:02:27 +0200

    updated hello js

--- scripts/hello-there.js
+++ scripts/hello-there.js
@@ -9,18 +9,19 @@
 // random hellos from miribot
 var hellosFromMiribot = [
   'https://media.giphy.com/media/yoJC2A59OCZHs1LXvW/giphy.gif',
-  'https://media2.giphy.com/media/VZjeyzBLSn3uE/200_s.gif',
+  'https://media.giphy.com/media/F1unFvakdEyqs/giphy.gif',
   'https://media.giphy.com/media/djRJNZqj508sE/giphy-facebook_s.jpg',
   'http://www.reactiongifs.com/r/dink.gif',
   'https://cdn.meme.am/instances/500x/58012475.jpg',
   'http://trollcats.com/wp-content/uploads/2010/07/la_la_la_i_cant_hear_you_trollcat-e1280290521584.jpg',
   'http://vampirediaries.com/files/2013/11/hello-there-gentle-viewers.gif',
   'That\'s me! :D',
-  'Whaaaaaaaats up?! :nerdface:'
+  'https://cdn.meme.am/instances/37564810.jpg',
+  'http://hydra-media.cursecdn.com/sto.gamepedia.com/a/a3/User_Zutty_Hello.png'
 ];
 
 module.exports = function(robot) {
-  robot.hear(/@miribot/, function(res) {
+  robot.hear(/<@miribot>/, function(res) {
     res.send(res.random(hellosFromMiribot));
   });
 };
