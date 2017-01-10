928d79736a66c15dcdc94fcdfbf05cfbff8c6922
programmiri <programmiri@gmail.com>
2017-01-10 13:10:45 +0100

    some more responses

--- scripts/slackbot-memorial.js
+++ scripts/slackbot-memorial.js
@@ -6,11 +6,15 @@
 
 var slackbotResponses = [
   'Rewe Rewe Rewe Rewe!!!!',
-  'You can\'t kill me!',
-  'Nice try, nice try!',
+  'You can\'t kill ME!',
+  'Nice try... nice try!',
   'RIP slackbot...',
   'We\'ll never forget you, slackbot!',
-  'Killing bots, aren\'t you?! Come at me bro!'
+  'Killing bots, aren\'t you?! Come at me bro!',
+  'Jan killed slackbot! https://media.giphy.com/media/DpB9NBjny7jF1pd0yt2/giphy.gif',
+  'http://i.imgur.com/6lKeqpd.png',
+  '#peniskuchen',
+  'http://i.imgur.com/kMCsqog.png'
 ];
 
 
